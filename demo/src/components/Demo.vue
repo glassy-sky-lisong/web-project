63e89c88f0670e6fdc4c4c5b7a4ac3945820b8e2
lisong <1990691193@qq.com>
2021-09-16 09:51:14 +0000

    2

---
+++ demo/src/components/Demo.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const incr = () => {
+  count.value++
+}
+
+const desc = () => {
+  count.value--
+}
+</script>
+
+<template>
+  <div class="couner">
+    <p>{{count}}</p>
+    <button @click="incr" >incr</button>
+    <button @click="desc">desc</button>
+  </div>
+</template>
